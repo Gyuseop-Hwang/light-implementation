5ccc1eb83d274a755507c17b386f35344a8573b7
Jensen-Hwang <rbtjqghkd@gmail.com>
2022-10-04 00:55:51 +0900

    addUser 모달창 구현 완료

--- AddUser/app.js
+++ AddUser/app.js
@@ -2,17 +2,65 @@ const addForm = document.querySelector('.form-box form');
 const nameInput = document.querySelector('#name')
 const ageInput = document.querySelector('#age')
 const userList = document.querySelector('.userlist ul')
+const modalEl = document.querySelector('div.modal');
+const template = document.querySelector('template')
+const backDrop = document.querySelector('.backdrop');
 
+
+
+let isError = false;
 // console.log(addForm)
 addForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const enteredName = nameInput.value;
   const enteredAge = ageInput.value;
-  if (enteredName.trim().length === 0) {
-
-    return;
+  if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+    const title = "올바르지 않은 입력입니다."
+    const message = "이름과 나이는 공백이여서는 안 됩니다."
+    isError = { title, message }
   }
-  if (enteredAge.trim().length === 0) {
+
+  else if (+enteredAge <= 0) {
+    const title = "올바르지 않은 입력입니다."
+    const message = "나이는 0보다 큰 양수여야 합니다."
+    isError = { title, message }
+
+  }
+
+  if (isError) {
+
+
+    const modalContent = document.importNode(template.content, true);
+
+    modalContent.querySelector('h3').textContent = isError.title;
+    modalContent.querySelector('p').textContent = isError.message;
+
+    modalEl.append(modalContent)
+
+    modalEl.classList.add('on');
+    backDrop.classList.add('on');
+
+    // modalEl.innerHTML = `
+    //   <h3>${isError.title}</h3>
+    //   <p>${isError.message}</p>
+    //   <div>
+    //     <button class="btn">확인</button>
+    //   </div>
+    // `
+    backDrop.addEventListener('click', (evt) => {
+      evt.currentTarget.classList.remove('on');
+      modalEl.classList.remove('on');
+      isError = false;
+      modalEl.innerHTML = '';
+    })
+
+    modalEl.querySelector('button').addEventListener('click', (evt) => {
+      modalEl.classList.remove('on');
+      isError = false;
+      modalEl.innerHTML = '';
+      backDrop.classList.remove('on');
+    });
+
     return;
   }
   const El = document.createElement('li');
@@ -24,3 +72,4 @@ addForm.addEventListener('submit', (evt) => {
   nameInput.value = "";
   ageInput.value = "";
 })
+
